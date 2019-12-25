cdc7d694a09932034e77604b4c04d16a0be297a4
XtremeGood <manglavishesh64@gmail.com>
2019-12-26 00:06:17 +0530

    Added npm added dependable

---
+++ container.js
@@ -0,0 +1,15 @@
+const dependable = require("dependable")
+const path = require('path');
+
+const container = dependable.container();
+const simpleDependencies = [
+['lodash', 'lodash'],
+] ;
+
+simpleDependencies.forEach(function(val){
+    container.register(val[0], function(){
+        return require(val[1]);
+    })
+});
+
+const lodash = require('lodash')
